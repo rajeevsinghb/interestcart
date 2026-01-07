4abdd14ac18a309b707e8c52e0a6477d1d28329d
Rajeev Kumar Singh Baghel <rajeevsinghb@gmail.com>
2026-01-07 23:02:41 +0530

    Added Disclaimer page

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -12,34 +12,31 @@ const Footer = () => (
         logo={<Logo />}
         iconList={<>{/* Social icons can be enabled later */}</>}
       >
+        {/* Primary */}
         <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
+          <Link href="/">Home</Link>
         </li>
 
         <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
+          <Link href="/about">About</Link>
+        </li>
+
+        {/* Legal & Trust */}
+        <li>
+          <Link href="/disclaimer">Disclaimer</Link>
         </li>
 
         <li>
-          <Link href="/privacy">
-            <a>Privacy Policy</a>
-          </Link>
+          <Link href="/privacy">Privacy Policy</Link>
         </li>
 
         <li>
-          <Link href="/terms">
-            <a>Terms of Service</a>
-          </Link>
+          <Link href="/terms">Terms of Service</Link>
         </li>
 
+        {/* Support */}
         <li>
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
+          <Link href="/contact">Contact</Link>
         </li>
       </CenteredFooter>
     </Section>
